81278816e3711df79193e28552bf51b4e900291c
dalginichev1@bk.ru <daalginichev@DESKTOP-9VBGCR6.localdomain>
2025-09-15 21:15:38 +0300

    Update src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,12 @@
 
 int main(int argc, char* argw[])
 {
+    int number = 0;
+
     while (std::cin >> number)
     {
         auto pair = closest_pair_tonum(number);
+        std::cout << "(" << pair.n << " , " << pair.m << ")" << "\n"; 
     }
     
     return 0;
